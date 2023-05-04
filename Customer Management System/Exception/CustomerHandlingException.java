22c1c67c44fb494d5582ba5fbfc5a614755e079e
TUSHARP0 <130496086+TUSHARP0@users.noreply.github.com>
2023-05-04 07:36:25 +0530

    Create CustmerHandlingException.java
    Custom Exception is created.

---
+++ Customer Management System/Exception/CustomerHandlingException.java
@@ -0,0 +1,9 @@
+package Exception;
+
+@SuppressWarnings("serial")
+public class CustomerValidationException extends Exception {
+	
+	public CustomerValidationException(String msg) {
+		super(msg);
+	}
+}
