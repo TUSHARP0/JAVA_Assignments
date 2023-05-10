dbc3d0a84593c867c1a4a4ae0c3e25bbe22d77f1
TUSHARP0 <130496086+TUSHARP0@users.noreply.github.com>
2023-05-10 07:02:13 +0530

    Create EmpTester.java

---
+++ Employee Management System/Tester/EmpTester.java
@@ -0,0 +1,67 @@
+package Tester;
+
+import java.util.Map;
+import java.util.Scanner;
+import static Utils.IOUtils.*;
+import com.app.core.Department;
+import com.app.core.Emp;
+import static Utils.ValidationRules.*;
+import static com.app.core.Data.*;
+public class EmpTester {
+
+	public static void main(String[] args) {
+		try(Scanner sc = new Scanner(System.in)){
+			Map<String,Emp> map=populatedMap();
+			boolean exit=false;
+			while(!exit) {
+				System.out.println("1. Hire Employee  2. Display Employees  3. Promote  4. Delete  5. Sort Employee as per join Date  0. Exit and store the file");
+				System.out.println("Enter choice");
+				try {
+				switch(sc.nextInt()){
+				case 1:
+					System.out.println("KeyId, id, FirstName, LastName, Department, Salary, Joining Date ");
+					Emp validate=validateAllInputs(sc.next(),sc.next(),sc.next(),sc.next(),sc.nextDouble(),sc.next(),map);
+					map.put(validate.getId(), validate);
+					System.out.println("Employee Hired");
+					break;
+				
+				case 2:
+					map.values().stream().forEach(e -> System.out.println(e));
+					break;
+				
+				case 3:
+					System.out.println("Enter employee Id");
+					String id= sc.next();
+					Department d=validateDept(sc.next());
+					if(map.containsKey(id)) {
+						map.get(id).setDept(d);
+					}
+					break;
+					
+				case 4:
+					System.out.println("Enter Employee Id");
+					String eid=sc.next();
+					if(map.containsKey(eid))
+						map.remove(eid);
+					break;
+					
+				case 5:
+					map.values().stream().sorted((e1,e2) -> e1.getJoinDate().compareTo(e2.getJoinDate())).forEach(System.out::println);
+					break;
+					
+				case 0:
+					exit=true;
+					break;
+					
+				}
+				}catch(Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
