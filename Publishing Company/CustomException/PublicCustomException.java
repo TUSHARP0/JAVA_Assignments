9604f3bd05d89a45439d238a2fa2bb14a5b25e9f
TUSHARP0 <130496086+TUSHARP0@users.noreply.github.com>
2023-05-19 07:59:41 +0530

    Create PublicCustomException.java

---
+++ Publishing Company/CustomException/PublicCustomException.java
@@ -0,0 +1,9 @@
+package CustomException;
+
+@SuppressWarnings("serial")
+public class PublishCustomException extends Exception {
+
+	public PublishCustomException(String msg) {
+		super(msg);
+	}
+}
